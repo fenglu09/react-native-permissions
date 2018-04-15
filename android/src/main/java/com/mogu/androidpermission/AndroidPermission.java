c4f08783f10cc3addcf4e397f3c65fcf35ee4822
Jason <fenglu09@gmail.com>
2018-04-15 23:58:24 +0800

    Android端添加检测权限的方法

--- android/src/main/java/com/mogu/androidpermission/AndroidPermission.java
+++ android/src/main/java/com/mogu/androidpermission/AndroidPermission.java
@@ -1,15 +1,27 @@
 package com.mogu.androidpermission;
 
 
+import android.Manifest;
+import android.app.AppOpsManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
+import android.util.Log;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
+import android.os.Process;
+import android.widget.Toast;
+
+import java.lang.reflect.Method;
+import java.security.PermissionCollection;
 
 public class AndroidPermission extends ReactContextBaseJavaModule {
 
@@ -22,6 +34,23 @@ public class AndroidPermission extends ReactContextBaseJavaModule {
     public String getName() {
         return "AndroidPermission";
     }
+
+    @ReactMethod
+    public void check(String permission, Promise promise) {
+
+        if (Build.VERSION.SDK_INT > 23) {
+            //  Android 6.0+
+            int result = ContextCompat.checkSelfPermission(getCurrentActivity(), permission);
+            promise.resolve(result == PackageManager.PERMISSION_GRANTED);
+        } else {
+            // Android 6.0 以下
+            int result = PermissionChecker.checkSelfPermission(getCurrentActivity(), permission);
+            promise.resolve(result == PermissionChecker.PERMISSION_GRANTED);
+
+        }
+
+    }
+
 
     /*
     跳转权限设置
@@ -70,7 +99,7 @@ public class AndroidPermission extends ReactContextBaseJavaModule {
 
     }
 
-    public void MEIZU_permission(Context context,  Promise promise) {
+    public void MEIZU_permission(Context context, Promise promise) {
 
         Intent intent = new Intent("com.meizu.safe.security.SHOW_APPSEC");
         intent.addCategory(Intent.CATEGORY_DEFAULT);
@@ -85,5 +114,6 @@ public class AndroidPermission extends ReactContextBaseJavaModule {
         }
 
     }
+
 
 }
