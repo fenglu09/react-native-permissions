1a0e2de2d9f06fb6ede2538a08d3dbbd3d244410
how <370969878@qq.com>
2018-08-23 17:31:14 +0800

    targetSdkVersion 26 权限检测修改

--- android/src/main/java/com/mogu/androidpermission/AndroidPermission.java
+++ android/src/main/java/com/mogu/androidpermission/AndroidPermission.java
@@ -2,9 +2,14 @@ package com.mogu.androidpermission;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.PermissionChecker;
 
@@ -24,24 +29,67 @@ public class AndroidPermission extends ReactContextBaseJavaModule {
         return "AndroidPermission";
     }
 
+//    @ReactMethod
+//    public void check(String permission, Promise promise) {
+//
+//        // 暂时解决权限问题
+//        int result = PermissionChecker.checkSelfPermission(getCurrentActivity(), permission);
+//        promise.resolve(result == PermissionChecker.PERMISSION_GRANTED);
+//        // if (Build.VERSION.SDK_INT > 23) {
+//        //     //  Android 6.0+
+//        //     int result = ContextCompat.checkSelfPermission(getCurrentActivity(), permission);
+//        //     promise.resolve(result == PackageManager.PERMISSION_GRANTED);
+//        // } else {
+//        //     // Android 6.0 以下
+//        //     int result = PermissionChecker.checkSelfPermission(getCurrentActivity(), permission);
+//        //     promise.resolve(result == PermissionChecker.PERMISSION_GRANTED);
+//
+//        // }
+//
+//    }
+
+    // targetSdkVersion 26 权限申请 start
     @ReactMethod
     public void check(String permission, Promise promise) {
+        //add by david 动态申请权限 start
+        try {
+            Context context = getCurrentActivity();
+            SharedPreferences sharedPreferences = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE);
+            boolean firstRequest = sharedPreferences.getBoolean(permission, true);
+            this.promise = promise;
+            int result = PermissionChecker.checkSelfPermission(getCurrentActivity(), permission);
+            if (result != PermissionChecker.PERMISSION_GRANTED) {
+                if (firstRequest) {
+                    ActivityCompat.requestPermissions(getCurrentActivity(), new String[]{permission},
+                            100);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean(permission, false);
+                    editor.commit();
+                } else {
+                    promise.resolve(result == PermissionChecker.PERMISSION_GRANTED);
+                }
+            } else {
+                promise.resolve(result == PermissionChecker.PERMISSION_GRANTED);
+            }
 
-        // 暂时解决权限问题
-        int result = PermissionChecker.checkSelfPermission(getCurrentActivity(), permission);
-        promise.resolve(result == PermissionChecker.PERMISSION_GRANTED);
-        // if (Build.VERSION.SDK_INT > 23) {
-        //     //  Android 6.0+
-        //     int result = ContextCompat.checkSelfPermission(getCurrentActivity(), permission);
-        //     promise.resolve(result == PackageManager.PERMISSION_GRANTED);
-        // } else {
-        //     // Android 6.0 以下
-        //     int result = PermissionChecker.checkSelfPermission(getCurrentActivity(), permission);
-        //     promise.resolve(result == PermissionChecker.PERMISSION_GRANTED);
-
-        // }
-
+        } catch (Exception e) {
+            promise.reject("-1", "检测失败");
+        }
+        //add by david 动态申请权限 end
+        //promise.resolve(result == PermissionChecker.PERMISSION_GRANTED);
     }
+
+    @ReactMethod
+    public void openNetWorkSettings(Promise promise) {
+        try {
+            getCurrentActivity().startActivity(new Intent(Settings.ACTION_SETTINGS));
+        } catch (Exception e) {
+            promise.reject("-1", "打开失败");
+        }
+    }
+
+    // targetSdkVersion 26 权限申请 end
+
 
     /*
     跳转权限设置
